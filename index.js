2cfbe7b0d5fac95402cf49927a4b1d184d25994a
Nianis Watanasiriwanich <nianis612@gmail.com>
2025-04-15 10:22:03 +0700

    FixPort

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("BOT ALIVE!!!");
+  res.send("BOT ALIVE!!");
 });
 
 app.listen(port, () => {
